2f894b0d7620e935d0efdfbd337a082e9a33c6f0
sanotogii <nasaereh@gmail.com>
2024-08-19 23:19:00 +0200

    task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import csv
 import math
-from typing import List
+from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     returns a tuple of size two
     """
