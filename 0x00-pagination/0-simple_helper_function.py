69ca47ac48fc8526291f53bdf0383d8f7c27fb86
sanotogii <nasaereh@gmail.com>
2024-08-19 21:52:27 +0200

    task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+
+
+def index_range(page:int, page_size:int) -> tuple:
+    """
+    returns a tuple of size two
+    """
+    return ((page - 1) * page_size, page * page_size)
