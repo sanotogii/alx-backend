406dc508b37f279f7ce42c6eac1e4495d6e86e88
sanotogii <nasaereh@gmail.com>
2024-08-21 21:51:59 +0200

    task 0+++

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -18,8 +18,8 @@ class BasicCache(BaseCaching):
         """
         if key is None or item is None:
             pass
-
-        self.cache_data[key] = item
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """
