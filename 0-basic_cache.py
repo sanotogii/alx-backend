4f502013903d31d579ee5b494af1dc6a7f9ccdb6
sanotogii <nasaereh@gmail.com>
2024-08-21 20:38:52 +0200

    task 0

---
+++ 0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from base_caching import BaseCaching
+"""
+basic caching system with no size limit
+"""
+
+
+class BasicCache(BaseCaching):
+    """
+    basic caching system with no size limit
+    """
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
