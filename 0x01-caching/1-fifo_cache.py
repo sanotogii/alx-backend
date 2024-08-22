1907d6e99963cb343ee499e8de4726a25d1877f8
sanotogii <nasaereh@gmail.com>
2024-08-22 03:30:40 +0200

    task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+FIFO caching system
+"""
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """
+    FIFO cashing system
+    """
+
+    def __init__(self):
+        super().__init__()
+    
+    def put(self, key, item):
+        """
+        add an item in the cache
+        """
+        if key is None or item is None:
+            pass
+        if len(self.cache_data) > self.MAX_ITEMS:
+            oldest_key = next(iter(self.cache_data))
+            del self.cache_data[oldest_key]
+            print(f"DISCARD: {oldest_key}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        get an item by key
+        """
+        return self.cache_data.get(key)
