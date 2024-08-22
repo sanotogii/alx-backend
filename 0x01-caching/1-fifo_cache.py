3fd65a8aa8cf7e0c2d070d5d869fff5a3fe74bce
sanotogii <nasaereh@gmail.com>
2024-08-22 03:41:01 +0200

    task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -12,18 +12,19 @@ class FIFOCache(BaseCaching):
 
     def __init__(self):
         super().__init__()
-    
+
     def put(self, key, item):
         """
         add an item in the cache
         """
         if key is None or item is None:
             pass
-        if len(self.cache_data) > self.MAX_ITEMS:
-            oldest_key = next(iter(self.cache_data))
-            del self.cache_data[oldest_key]
-            print(f"DISCARD: {oldest_key}")
-        self.cache_data[key] = item
+        else:
+            if len(self.cache_data) > self.MAX_ITEMS:
+                oldest_key = next(iter(self.cache_data))
+                del self.cache_data[oldest_key]
+                print(f"DISCARD: {oldest_key}")
+            self.cache_data[key] = item
 
     def get(self, key):
         """
