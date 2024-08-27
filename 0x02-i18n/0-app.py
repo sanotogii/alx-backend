35385f5f4c2b8e33f1098452222d02c5fad056a3
sanotogii <nasaereh@gmail.com>
2024-08-27 14:57:34 +0200

    Basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This script starts a Flask web application
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/0-index.html', strict_slashes=False)
+def hello_world():
+    """
+    This function renders the
+    0-index.html file
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
